91eeb616b83171490d90b5cae85085596bff6b8b
Chlaxi <chlaxi.lunari@gmail.com>
2019-11-01 16:04:53 +0100

    Added Webservice

---
+++ WebApplication/Controllers/CategoriesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        [Route("api/categories")]
+        public  string Index()// IActionResult Index()
+        {
+            return "Default test";// View();
+        }
+    }
+}
---
+++ WebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Assignment4;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductsController : Controller
+    {
+        private DataService ds = new DataService();
+
+
+        public string Index()  //ActionResult Index()
+        {
+
+            return "Product Index";   //View();
+        }
+
+
+        //Get all products TODO: Refactor
+        [HttpGet]
+        public ActionResult GetProducts()
+        {
+            using var db = new NorthwindContext();
+                        
+            return Ok(db.Products.ToArray());
+        }
+
+        [HttpGet("{productId}")]
+        public ActionResult GetProduct(int productId)
+        {
+            var product = ds.GetProduct(productId);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+    }
+}
