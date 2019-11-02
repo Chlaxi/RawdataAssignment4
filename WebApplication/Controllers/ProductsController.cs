9acd3cd208fe1cc1b5def797517f99c925a86668
Chlaxi <chlaxi.lunari@gmail.com>
2019-11-02 14:58:55 +0100

    Added a DTO for products, as well as implementing functionality for it.

--- WebApplication/Controllers/ProductsController.cs
+++ WebApplication/Controllers/ProductsController.cs
@@ -22,29 +22,35 @@ namespace WebApplication.Controllers
         }*/
 
 
-        [HttpGet]
+        [HttpGet(Name = nameof(GetProducts))]
         public ActionResult GetProducts()
         {
             List<Product> products = ds.GetProducts();
             if (products.Count == 0) return NotFound();
-            return Ok(products);
+
+            List<ProductDTO> productDTOs = GetDTOList(products);
+
+            return Ok(productDTOs);
         }
 
-        [HttpGet("{productId}")]
-        public ActionResult GetProduct(int productId)
+        [HttpGet("{id}", Name = nameof(GetProduct))]
+        public ActionResult GetProduct(int id)
         {
-            var product = ds.GetProduct(productId);
+            var product = ds.GetProduct(id);
             if (product == null) return NotFound();
-            return Ok(product);
+
+            return Ok(CreateProductDTO(product));
         }
 
-        [HttpGet("text/{query}")]
+        [HttpGet("name/{query}")]
         public ActionResult SearchProduct(string query)
         {
             List<Product> products = ds.GetProductByName(query);
-            if (products.Count == 0) return NotFound();
+            if (products.Count == 0) return NotFound(products);
+            
+            List<ProductDTO> productDTOs = GetDTOList(products);
 
-            return Ok(products);
+            return Ok(productDTOs);
         }
 
 
@@ -53,11 +59,41 @@ namespace WebApplication.Controllers
         {
             List<Product> products = ds.GetProductByCategory(cat);
             Console.WriteLine(products.Count);
-            if (products.Count == 0) return NotFound();
-            
-            return Ok(products);
-        
+            if (products.Count == 0) return NotFound(products);
+
+            List<ProductDTO> productDTOs = GetDTOList(products);
+            return Ok(productDTOs);       
         }
+
+
+        public ProductDTO CreateProductDTO(Product product)
+        {
+
+            ProductDTO dto = new ProductDTO();
+            dto.Link = Url.Link(
+                    nameof(GetProduct),
+                    new { id = product.Id });
+            dto.Name = product.Name;
+            dto.ProductName = product.Name;
+            dto.QuantityPerUnit = product.QuantityPerUnit;
+            dto.UnitPrice = product.UnitPrice;
+            dto.UnitsInStock = product.UnitsInStock;
+            dto.Category = product.Category;         
+
+            return dto;
+        }
+
+        private List<ProductDTO> GetDTOList(List<Product> products)
+        {
+            List<ProductDTO> DTO = new List<ProductDTO>();
+            foreach (var prod in products)
+            {
+                DTO.Add(CreateProductDTO(prod));
+            }
+            return DTO;
+        }
+
+
     }
 
 }
---
+++ WebApplication/Models/ProductDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment4;
+
+namespace WebApplication
+{
+    public class ProductDTO
+    {
+        public string Link { get; set; }
+        public string Name { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public int UnitsInStock { get; set; }
+
+        public Category Category { get; set; }
+
+        public string CategoryName { get { return Category.Name; } }
+    }
+}
